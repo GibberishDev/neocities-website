8363f81edb04fa74a0bc3d8eee1920ed50643ae1
GibberishDev <GibberishDevelopment@gmail.com>
2026-08-05 01:52:43 +0300

    implement loading screen

---
+++ scripts/bootloader.js
@@ -0,0 +1,11 @@
+setTimeout(()=>{
+    document.getElementById("startup-boot-text").remove()
+    document.getElementById("boot-splash").style.display = ""
+    setTimeout(()=>{
+    document.getElementById("gibb-os-startup-loading-screen").style.opacity = "0"
+    },5500)
+    setTimeout(()=>{
+    document.getElementById("gibb-os-startup-loading-screen").remove()
+    },10000)
+},1500)
+// TODO: replace progress bar with actual loading to ensure all assets get downloaded
