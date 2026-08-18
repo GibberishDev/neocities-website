f63beca8ba8e6b2bb1e838b4cfc9de31cfce8ff4
GibberishDev <GibberishDevelopment@gmail.com>
2026-08-18 21:28:25 +0300

    console spam

---
+++ scripts/console.js
@@ -0,0 +1,12 @@
+setInterval(()=>{
+    sendMessage()
+},2000)
+function sendMessage() {
+    console.clear()
+    console.log("%cSTOP RIGHT THERE","background:black;color:red;font-size:2rem;font-weight:bold")
+    console.log("%cThis ARG/Game isn't designed around users poking around the code of the webpage.","background:black;color:red")
+    console.log("%cI don't really want to mess around with setting up secure server for this so be good and","background:black;color:red")
+    console.log("%cSTOP LOOKING AT DEV CONSOLE","background:black;color:red;font-size:1.5rem;font-weight:bold")
+    console.log("%cUnless you %cREALLY %cwant to spoil yourself everything and datamine this. I will not respect you.","background:black;color:red;font-size:1rem","background:black;color:white;font-style:italic;font-weight:bold","background:black;color:red")
+}
+sendMessage()
